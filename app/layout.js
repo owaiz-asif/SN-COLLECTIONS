881520868d206a60f11bcc22775a6a7de3fc21bd
owaiz-asif <asifowaiz50@gmail.com>
2026-04-12 18:54:31 +0530

    added google verification

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script dangerouslySetInnerHTML={{__html:'window.addEventListener("error",function(e){if(e.error instanceof DOMException&&e.error.name==="DataCloneError"&&e.message&&e.message.includes("PerformanceServerTiming")){e.stopImmediatePropagation();e.preventDefault()}},true);'}} />
       </head>
       <body className="antialiased">
         {children}
